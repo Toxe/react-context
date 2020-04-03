858d8e6dc6e854ee5147c6b1c95e036e3475a851
Toxe <toxe@toxe.net>
2020-04-03 15:19:48 +0200

    Example container hierarchy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import UserContainer from "./User";
 
 export default class App extends Component {
     render() {
-        return <div>App</div>;
+        const users = [
+            {
+                id: 0,
+                name: "John"
+            },
+            {
+                id: 1,
+                name: "James"
+            },
+            {
+                id: 2,
+                name: "Jim"
+            }
+        ];
+
+        return (
+            <div>
+                <UserContainer user={users[0]} />
+                <UserContainer user={users[1]} />
+                <UserContainer user={users[2]} />
+            </div>
+        );
     }
 }
---
+++ src/User.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./User.css";
+
+export default function UserContainer(props) {
+    return (
+        <div className="UserContainer">
+            <div className="title">UserContainer</div>
+            <UserContainerContent user={props.user} />
+        </div>
+    );
+}
+
+function UserContainerContent(props) {
+    return (
+        <div className="UserContainerContent">
+            <div className="title">UserContainerContent</div>
+            <UserContainerBody user={props.user} />
+        </div>
+    );
+}
+
+function UserContainerBody(props) {
+    return (
+        <div className="UserContainerBody">
+            <div className="title">UserContainerBody</div>
+            <UserInfo user={props.user} />
+        </div>
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <div className="title">UserInfo</div>
+            <div>User id: {props.user.id}</div>
+            <div>User name: {props.user.name}</div>
+        </div>
+    );
+}
